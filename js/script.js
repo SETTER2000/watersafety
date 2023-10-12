c90dbb55d74aa72424bda91e94e50f8da41d2261
setter <lphp@mail.ru>
2023-10-12 19:34:26 +0300

    Init App

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#head').load("/pages/head.html");
+    $('#sidebar').load("/pages/sidebar.html");
+    $('#header').load("/pages/header.html");
+    $('#footer').load("/pages/footer.html");
+
+    $('#hd').load("hd.html");
+}) ;
+
