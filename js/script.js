a571aa22cfb284965d0772a38e08580a8ed951f2
setter <lphp@mail.ru>
2023-10-13 18:43:06 +0300

    New HTML version

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $('#head').load("/pages/head.html");
-    $('#sidebar').load("/pages/sidebar.html");
-    $('#header').load("/pages/header.html");
-    $('#footer').load("/pages/footer.html");
+    $('#head').load("head.html");
+    $('#sidebar').load("sidebar.html");
+    $('#header').load("header.html");
+    $('#footer').load("footer.html");
     $('#hd').load("hd.html");
 }) ;
 
