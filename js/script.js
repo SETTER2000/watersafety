a07af0eab7e91b0d1f25cdf967e14f4efd54fc86
setter <lphp@mail.ru>
2023-10-13 12:09:05 +0300

    change js script

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
     $('#head').load("/pages/head.html");
-    $('#navi').load("/pages/navi.html");
     $('#sidebar').load("/pages/sidebar.html");
     $('#header').load("/pages/header.html");
     $('#footer').load("/pages/footer.html");
-
     $('#hd').load("hd.html");
 }) ;
 
